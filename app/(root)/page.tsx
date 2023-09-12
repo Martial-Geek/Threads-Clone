a49d4f4e392802159e5179ffe2a79177329e76ff
Uwais Jawed <uwaisjawed8081@gmail.com>
2023-09-12 19:08:16 +0530

    Auth Complete, Home Page Layout Complete

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+ 
+export default function Home() {
+  return (
+    <div>
+      {/* <UserButton afterSignOutUrl="/"/> */}
+      <h1 className="head-text">Home</h1>
+    </div>
+  )
+}
+
