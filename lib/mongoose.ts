88ed3843dd5e1d6b3f1d0687479af961ef9384c8
Uwais Jawed <uwaisjawed8081@gmail.com>
2023-09-16 13:39:22 +0530

    Onboarding Done

---
+++ lib/mongoose.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(!process.env.MONGODB_URL) return console.log('Mongo DB URL not found!');
+
+    if(isConnected) return console.log('Already connected to Mongo DB');
+    
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        isConnected = true;
+
+        console.log('Connected to MongoDB');
+        
+    } catch (error) {
+        console.log(error);
+    } 
+}
